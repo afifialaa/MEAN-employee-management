e8455ad801f4ea3e4dc6eda01c09d0ebe4cd5308
alaa afifi <alaaahmedafifi@gmail.com>
2022-02-12 20:04:51 +0200

    database connection iife

--- database/config.js
+++ database/config.js
@@ -1,47 +1,55 @@
 const mongoose = require('mongoose');
 
-if(process.env.NODE_ENV == 'development'){
-    console.log('\x1b[32m[server] running in development mode');
-    connect(process.env.MONGODB_DEV)
-}
+let Database = (() => {
+    let connection
+    let connectionString
 
-if(process.env.NODE_ENV == 'testing'){
-    console.log('\x1b[32m[server] running in test mode');
-    connect(process.env.MONGODB_TEST)
-}
+    if (process.env.NODE_ENV == 'development') {
+        connectionString = process.env.MONGODB_DEV;
+        connect(connectionString)
+    }
 
-if(process.env.NODE_ENV == 'production'){
-    console.log('[server] running in production mode');
-    connect(process.env.MONGODB_PROD)
-}
+    if (process.env.NODE_ENV == 'testing') {
+        connectionString = process.env.MONGODB_TEST;
+    }
 
+    if (process.env.NODE_ENV == 'production') {
+        connectionString = process.env.MONGODB_PROD;
+    }
 
-/**
- * Establishes connection to database
- */
-function connect(connectionString){
-    mongoose.connect(connectionString, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
+    async function connect(connString) {
+        try {
 
-    let db = mongoose.connection;
+            mongoose.connection.on('open', () => {
+                console.log('[database] connection to database is established \x1b[0m');
+            })
+        
+            mongoose.connection.on('error', () => {
+                console.log('\x1b[41m [database] failed to connect to database \x1b[0m');
+            })
 
-    db.on('open', () => {
-        console.log('[database] connection to database is established \x1b[0m');
-    })
+            mongoose.connection.on('close', ()=>{
+                console.log('\x1b[41m [database] connection to database is terminated \x1b[0m');
+            })
 
-    db.on('error', () => {
-        console.log('\x1b[41m [server] failed to connect to database \x1b[0m');
-    })
+            connection = mongoose.connect(connString, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
 
-    db.on('close', ()=>{
-        console.log('[database] connection to database is terminated')
-    })
-}
+        }catch(err){
+            console.log(err)
+        }
+        
+    }
 
-function close(){
-    mongoose.connection.close()
-}
+    return {
+        getConnectionString: function(){
+            return connectionString
+        },
 
-module.exports = {
-    connect,
-    close
-}
+        getInstance: function () {
+            if (!connection) {
+                connect();
+            }
+            return connection;
+        }
+    }
+})()
