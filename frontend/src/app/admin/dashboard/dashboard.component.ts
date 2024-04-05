73612e945daee8b95aee1a9f1a692e3594a1dbaa
Alaa Afifi <alaaafifi@192.168.1.3>
2024-04-05 16:37:49 +0200

    user card update

--- frontend/src/app/admin/dashboard/dashboard.component.ts
+++ frontend/src/app/admin/dashboard/dashboard.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
 
-  employeeTitle = 'Employee'
-  employeeData = 100
-  employeeUrl = '/admin/employee/dashboard'
+    employeeTitle = 'Employee'
+    employeeData = 100
+    employeeUrl = '/admin/employee/dashboard'
+
+    userTitle = 'User'
+    userData = 100
+    userURL = '/admin/user'
 
 
-  inventoryTitle = 'Inventory'
-  inventoryData = 100
-  inventoryUrl = '/admin/inventory'
+    inventoryTitle = 'Inventory'
+    inventoryData = 100
+    inventoryUrl = '/admin/inventory'
 }
